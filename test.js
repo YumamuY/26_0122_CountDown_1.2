76188c9aa824a936042a055d9a012e04a8e25709
YumamuY <yamamoto.yuma.t1@dc.tohoku.ac.jp>
2026-02-15 17:50:49 +0900

    26_0215_practice

--- test.js
+++ test.js
@@ -1,2 +1,30 @@
-const now = new Date();
-console.log(now);
+// What is forEach()?
+// forEach() is a method for arrays that lets you run a function once for each element in the array.
+
+const numbers = [1, 2, 3];
+numbers.forEach((num) => {
+    console.log(num);
+})
+
+const japanese = ["い", "ろ", "は", "に", "ほ", "へ", "と"];
+japanese.forEach((character) => {
+    console.log(character);
+})
+
+
+// What is .dataset in HTML?
+// In HTML, you can create custom attributes using data-*.
+// .dataset is a way to attach invisible metadate to a DOM element
+const yuma = document.getElementById("yuma");
+const angel = document.getElementById("angel");
+yuma.dataset.id = angel.id
+console.log(yuma.dataset.id);
+
+
+// What is ?
+
+// What is ?
+
+// What is ?
+
+// What is ?
